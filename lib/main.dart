1016935de5ee3e610941af414c2f49b4a419af45
Gabriel Carlos <gabrielc@dbserver.com.br>
2020-09-25 14:38:45 -0300

    Certificados

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,21 @@ void main() async {
 }
 
 Future<bool> addSelfSignedCertificate() async {
-  ByteData data = await rootBundle.load('assets/certs/raiz.crt');
+  ByteData data = await rootBundle.load('assets/certs/autoridade_sicredi.der');
   SecurityContext context = SecurityContext.defaultContext;
   context.setTrustedCertificatesBytes(data.buffer.asUint8List());
 
-  ByteData data2 = await rootBundle.load('assets/certs/intermediario.crt');
-  context.setTrustedCertificatesBytes(data2.buffer.asUint8List());
+  // ByteData data2 = await rootBundle.load('assets/certs/intermediario.crt');
+  // context.setTrustedCertificatesBytes(data2.buffer.asUint8List());
 
-  ByteData data3 = await rootBundle.load('assets/certs/intermediario2.crt');
-  context.setTrustedCertificatesBytes(data3.buffer.asUint8List());
+  // ByteData data3 = await rootBundle.load('assets/certs/intermediario2.crt');
+  // context.setTrustedCertificatesBytes(data3.buffer.asUint8List());
 
-  ByteData data4 = await rootBundle.load('assets/certs/intermediario3.crt');
-  context.setTrustedCertificatesBytes(data4.buffer.asUint8List());
+  // ByteData data4 = await rootBundle.load('assets/certs/intermediario3.crt');
+  // context.setTrustedCertificatesBytes(data4.buffer.asUint8List());
 
-  ByteData data5 = await rootBundle.load('assets/certs/intermediario4.crt');
-  context.useCertificateChainBytes(data5.buffer.asUint8List());
+  // ByteData data5 = await rootBundle.load('assets/certs/intermediario4.crt');
+  // context.useCertificateChainBytes(data5.buffer.asUint8List());
   return true;
 }
 
